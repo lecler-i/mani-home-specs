ca44040569cde971589fe878b5abd77d1789a988
Thomas LECLERCQ <degnus@gmail.com>
2017-04-12 01:24:06 +0530

    Rename typo

--- scripts/generate_srs.sh
+++ scripts/generate_srs.sh
@@ -1,3 +1,3 @@
 !# /usr/bin/env sh
 cd srs/
-pandoc -o ../srs.pdf --latex-engine xelatex --toc -V documentclass=report --chapters *.md
+pandoc -o ../srs.pdf --latex-engine xelatex --toc --top-level-division=chapter *.md
