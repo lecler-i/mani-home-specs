9225528751be93a327ead91e70ee951c9100f5f1
Thomas LECLERCQ <degnus@gmail.com>
2017-05-15 16:51:12 +0530

    Add gulpfile + package.json

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var pdf = require('gulp-pandoc-pdf');
+var plantuml = require('gulp-plantuml');
+var watch = require('gulp-watch');
+var plumber = require('gulp-plumber');
+var sort = require('gulp-sort');
+
+gulp.task('build', ['uml', 'pdf']);
+
+var src = 'src/**/*.md';
+
+gulp.task('pdf', function() {
+  gulp.src(src)
+    .pipe(sort())
+    .pipe(plumber({
+        errorHandler: function (err) {
+          console.error(err.message);
+          this.emit('end');
+        }
+    }))
+    .pipe(concat('final-report.md'))
+    .pipe(pdf({
+      pdfDir: 'build',
+      args: ['--latex-engine', 'xelatex', '--data-dir=build', '-S', '--filter',  'pandoc-plantuml-filter', '-V', 'documentclass=report', '--template=template.ltx']
+    }))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('uml', function() {
+  return gulp.src('diagrams/*.puml')
+    .pipe(plantuml({
+      jarPath: '/opt/plantuml/plantuml.jar',
+    }))
+    .pipe(gulp.dest('build/uml/'));
+});
+
+gulp.task('watch', function () {
+  return watch(src, { ignoreInitial: true, verbose: true }, function() {
+    gulp.start('pdf');
+  })
+});
